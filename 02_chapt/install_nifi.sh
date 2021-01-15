0bf199b9713ff2be66585c2276353f069d990e3d
adhamlingithub@gmail.com <adhamlingithub@gmail.com>
2021-01-15 00:21:57 +0000

    added echos to script for user

--- 02_chapt/install_nifi.sh
+++ 02_chapt/install_nifi.sh
@@ -6,20 +6,27 @@
 # Download, install and confure Apache NiFi:
 echo '[*] Downloading Apache Nifi 1.12.1 to current directory...'
 wget https://mirrors.ocf.berkeley.edu/apache/nifi/1.12.1/nifi-1.12.1-bin.tar.gz
+
 # Extract Apache Nifi tar:
+echo '[*] Extracting Apache NiFi tar file...'
 tar xvzf nifi-1.12.1-bin.tar.gz
 
 # Install openjdk-8-jre-headless:
+echo '[*] Checking for system package information updates...'
 sudo apt update
+echo '[*] Installing openjdk-8-jre-headless...'
 sudo apt install openjdk-8-jre-headless -y
 
 # Set JAVA_HOME:
+echo '[*] Setting JAVA_HOME...'
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 # reload bash:
+echo '[*] Reloading bash terminal...'
 source .bashrc
 
 # Run nifi, in the nifi folder:
+echo '[*] Starting Apache NiFi...if successful GO >> http://localhost:8080/nifi/'
 nifi-1.12.1/bin/nifi.sh start
 
 # Test the NiFi local web setup (NOT for HEADLESS systems)
